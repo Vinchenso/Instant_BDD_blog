8b52397abaf3306710877b97edf4baeb67bf32d4
Vinchenso <chensok@gmail.com>
2016-02-27 17:28:19 +0200

    Story 1 completed

---
+++ features/step_definitions/write_post.rb
@@ -0,0 +1,26 @@
+Given(/^I am on the blog homepage$/) do
+  visit("/posts")
+end
+
+When(/^I click "([^"]*)" link$/) do |arg1|
+  click_on arg1
+end
+
+When(/^I fill "([^"]*)" as Title$/) do |title|
+  @title = title
+  fill_in "Title", :with => title
+end
+
+When(/^I fill "([^"]*)" as content$/) do |content|
+  fill_in "Content", :with => content 
+end
+
+When(/^I click "([^"]*)" button$/) do |btn|
+  click_button btn
+end
+
+Then(/^I should see the blog I just posted$/) do
+  page.has_content(@title)
+end
+
+
